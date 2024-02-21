081348e108768c242160e79527d3cd17257b4456
jhonnatan uruena <jhonnatan.uruena@sinco.com.co>
2024-02-21 18:14:18 -0500

    :refactor connection hub

--- Client/investigacion/src/hub/docusignHub.ts
+++ Client/investigacion/src/hub/docusignHub.ts
@@ -1,28 +1,34 @@
 import * as signalR from "@microsoft/signalr";
+import { SwipeableDrawer } from "@mui/material";
 
 const userId = "userId123";
 const additionalParam = "paramValue";
 const urlServer_HUB = `https://localhost:7028/docusign-hub?userId=${userId}&additionalParam=${additionalParam}`
 
-
-
-export const startConnection = async () => {
-
+const initialConecction = () => {
     const hubConnection = new signalR.HubConnectionBuilder()
         .withUrl(urlServer_HUB)
         .configureLogging(signalR.LogLevel.Information)
         .build();
 
+    return hubConnection;
+}
+
+export const startConnection = async () => {
+
 
     try {
+        const wss = initialConecction();
 
-
-        hubConnection.on("ReceiveMessage", (e) => {
+        wss.on("ReceiveMessage", (e) => {
 
             console.log(e);
         });
 
-        await hubConnection.start();
+        await wss.start();
+
+        
+
         console.log("Conexión con SignalR establecida.");
     } catch (err) {
         console.error("Error al conectar con SignalR:", err);
